e6c6c72a2384256ac06eb0164994b01898f92887
tanvir-tech <tanvirah325@gmail.com>
2021-04-06 00:25:05 +0600

    Split_Polycarp

---
+++ src/Split_Polycarp.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Split_Polycarp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String enc = sc.next();
+
+
+//        String enc = "110111001";
+
+        String[] Digit = enc.split("0");
+
+        String resultString ="";
+        for (String d:Digit) {
+            resultString = resultString + d.length();
+        }
+        System.out.println(Integer.parseInt(resultString));
+    }
+}
